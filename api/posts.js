f00cc6b8237ce4c28e66de8a3794155abc0b146b
chhotan <chhotan.mondal94@gmail.com>
2022-02-15 12:30:06 +0530

    footer api done

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const bodyParser = require("body-parser");
-
+const mongoose = require('mongoose');
 const Post = require("../model/Post");
 
 
@@ -13,6 +13,35 @@ router.get("/all-posts", async (req, res) => {
       res.status(500);
     }
 });
+
+router.get("/footer_details", async (req, res) => {
+    try{
+
+        var footers={};
+        const db1 = mongoose.connection.db;
+
+    //   await  db1.collection('social_link').find({link :{$ne:''}}).sort({priority: 1}).toArray(function(err, results){
+            
+    //     footers['links']=results;
+    //     //footers['footer_text']= "Copyright © 2022 All Right Reserved.";
+    //     var foot= db1.collection('footer').findOne({uid:"1"}); 
+    //      footers['footer_text']=foot.footer_text;
+
+    //    res.send(footers);
+    //     });
+        
+        var links= await  db1.collection('social_link').find({link :{$ne:''}}).sort({priority: 1}).toArray();
+        var footer_text= await db1.collection('footer').findOne({uid:"1"}); 
+        footers['links']=links;
+        footers['footer_text']= footer_text.footer_text;
+
+        res.send(footers);
+        
+    } catch(error){
+      res.status(500);
+    }
+});
+
 
 
 router.post("/add-post", async (req, res) => {
